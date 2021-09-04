bf398daf1c151a662cfd0a8f633ee5b6895ad92d
Misha Mosi <mihmosi@gmail.com>
2021-09-04 16:21:29 +0300

    bug fix play list

--- src/main/java/misha/MusicPlayer.java
+++ src/main/java/misha/MusicPlayer.java
@@ -1,10 +1,18 @@
 package misha;
 
-public class MusicPlayer {
-    private Music music;
+import java.util.ArrayList;
+import java.util.List;
 
-    private  String name;
-     private int value;
+public class MusicPlayer {
+    //    private Music music;
+    private List<Music> musicList = new ArrayList<>();
+
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
+    }
+
+    private String name;
+    private int value;
 
     public String getName() {
         return name;
@@ -23,17 +31,21 @@ public class MusicPlayer {
     }
 
     // IoC
-    public MusicPlayer(Music music) {
-        this.music = music;
+    public MusicPlayer(List<Music> musicList) {
+        this.musicList = musicList;
     }
 
-    public MusicPlayer(){}
-
-    public void setMusic(Music music){
-        this.music = music;
+    public MusicPlayer() {
     }
 
-    public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+//    public void setMusic(Music music) {
+//        this.music = music;
+//    }
+
+    public void playMusicList() {
+        for (Music song: musicList) {
+            System.out.println("Playing: " + song.getSong());
+        }
     }
+
 }
