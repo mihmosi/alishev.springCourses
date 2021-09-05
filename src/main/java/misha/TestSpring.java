b8931bbb04637833da9fd0357a52a28f24b0b3b7
Misha Mosi <mihmosi@gmail.com>
2021-09-05 16:09:32 +0300

    bean init-mathod and destroy-method in scope prototype

--- src/main/java/misha/TestSpring.java
+++ src/main/java/misha/TestSpring.java
@@ -7,28 +7,37 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"    //выводим в контекст адрес конфигурации
         );
+        ClassicalMusic classicalMusic
+                = context.getBean("musicBean3", ClassicalMusic.class);
+
+        ClassicalMusic classicalMusic2
+                = context.getBean("musicBean3", ClassicalMusic.class);
+
+        System.out.println(classicalMusic.getSong());
+        System.out.println(classicalMusic2.getSong());
+
         /* получаем из контекста то что реализует интерфейс Music */
 //        Music music = context.getBean("musicBean", Music.class);
 //        MusicPlayer musicPlayer = new MusicPlayer(music);
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
-
-        boolean comparsion = musicPlayer == musicPlayer1;
-
-        musicPlayer.playMusicList();
-        System.out.println(comparsion);
-
-        System.out.println(musicPlayer);
-        System.out.println(musicPlayer1);
+//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
+//
+//        boolean comparsion = musicPlayer == musicPlayer1;
+//
+//        musicPlayer.playMusicList();
+//        System.out.println(comparsion);
+//
+//        System.out.println(musicPlayer);
+//        System.out.println(musicPlayer1);
 
 //        System.out.println(musicPlayer.getName());
 //        System.out.println(musicPlayer.getValue());
 
-        musicPlayer.setValue(10);
-
-        System.out.println(musicPlayer.getValue());
-        System.out.println(musicPlayer1.getValue());
+//        musicPlayer.setValue(10);
+//
+//        System.out.println(musicPlayer.getValue());
+//        System.out.println(musicPlayer1.getValue());
 
         context.close();
     }
